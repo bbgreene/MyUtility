a242ca2e726d21bd624e590a8ee8bb0b870e7d00
bbgreene <brian.greene.music@gmail.com>
2022-05-30 13:01:49 +0200

    created other parameter attachments

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -23,6 +23,29 @@ MyUtilityAudioProcessorEditor::MyUtilityAudioProcessorEditor (MyUtilityAudioProc
     delayAttachement = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.treeState, "delay", delay);
     delay.setDialStyle(bbg_gui::bbg_Dial::DialStyle::kDialDotModernStyle);
     addAndMakeVisible(delay);
+    
+    balanceAttachement = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.treeState, "balance", balance);
+    balance.setDialStyle(bbg_gui::bbg_Dial::DialStyle::kDialDotModernStyle);
+    balance.panDialSuffixChange(balance);
+    addAndMakeVisible(balance);
+    
+    widthAttachement = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.treeState, "width", width);
+    width.setDialStyle(bbg_gui::bbg_Dial::DialStyle::kDialDotModernStyle);
+    addAndMakeVisible(width);
+    
+    phaseAttachement = std::make_unique<juce::AudioProcessorValueTreeState::ButtonAttachment>(audioProcessor.treeState, "phase", phase);
+    phase.setColour(juce::ToggleButton::ColourIds::tickDisabledColourId, CustomColours::blackGrey);
+    phase.setColour(juce::ToggleButton::ColourIds::tickColourId, CustomColours::blue);
+    addAndMakeVisible(phase);
+
+    muteAttachement = std::make_unique<juce::AudioProcessorValueTreeState::ButtonAttachment>(audioProcessor.treeState, "mute", mute);
+    addAndMakeVisible(mute);
+    
+    monoAttachement = std::make_unique<juce::AudioProcessorValueTreeState::ButtonAttachment>(audioProcessor.treeState, "mono", mono);
+    addAndMakeVisible(mono);
+    
+//    bypassAttachement = std::make_unique<juce::AudioProcessorValueTreeState::ButtonAttachment>(audioProcessor.treeState, "bypass", bypass);
+    addAndMakeVisible(bypass);
 
     setSize (300, 600);
 }
@@ -35,7 +58,7 @@ MyUtilityAudioProcessorEditor::~MyUtilityAudioProcessorEditor()
 void MyUtilityAudioProcessorEditor::paint (juce::Graphics& g)
 {
     
-    g.fillAll(juce::Colours::black);
+    g.fillAll(juce::Colours::grey);
 
 }
 
@@ -43,8 +66,17 @@ void MyUtilityAudioProcessorEditor::resized()
 {
     auto bigDialSize = 200;
     auto smallDialSize = 150;
+    auto buttonWidth = smallDialSize / 2;
+    auto buttonHeight = smallDialSize / 3;
     auto middle = getWidth() / 6;
     
     gain.setBounds(middle, getHeight() - 500, bigDialSize, bigDialSize);
     delay.setBounds(0, gain.getBottom(), smallDialSize, smallDialSize);
+    phase.setBounds(delay.getRight(), gain.getBottom() + buttonHeight, buttonWidth, buttonHeight);
+    mute.setBounds(delay.getRight() + buttonWidth, gain.getBottom(), buttonWidth, buttonHeight);
+    mono.setBounds(delay.getRight() + buttonWidth, gain.getBottom() + buttonHeight, buttonWidth, buttonHeight);
+    bypass.setBounds(delay.getRight() + buttonWidth, gain.getBottom() + buttonHeight * 2, buttonWidth, buttonHeight);
+    balance.setBounds(0, delay.getBottom(), smallDialSize, smallDialSize);
+    width.setBounds(smallDialSize, delay.getBottom(), smallDialSize, smallDialSize);
+    
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -34,11 +34,23 @@ private:
     //UI dials and Buttons
     bbg_gui::bbg_Dial gain { " dB", -66.0, 24.0, 0.1, 0.0, 0.0 };
     bbg_gui::bbg_Dial delay { " Smps", 0.0, 48000.0, 1.0, 0.0, 0.0 };
+    bbg_gui::bbg_Dial balance { " C", -1.0, 1.0, 0.1, 0.0, 0.0 };
+    bbg_gui::bbg_Dial width { " W", 0.0, 5.0, 0.1, 1.0, 1.0 };
+    bbg_gui::bbg_PushButton mute { "Mute" };
+    bbg_gui::bbg_PushButton mono { "Mono" };
+    bbg_gui::bbg_PushButton bypass { "Bypass" };
+    bbg_gui::bbg_Toggle phase { "Phase" };
     
     //Attachments
-    
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> gainAttachement;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> delayAttachement;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> balanceAttachement;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> widthAttachement;
+    
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> phaseAttachement;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> muteAttachement;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> monoAttachement;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> bypassAttachement;
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     MyUtilityAudioProcessor& audioProcessor;
